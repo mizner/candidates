c658b7664bb7d13e1b80ffd48a9aadc6060a7610
Mizner <miz@Mizners-MacBook-Pro.local>
2018-01-08 11:30:10 -0500

    add target blank compat to link

--- content/themes/musicnow/template-parts/hero.php
+++ content/themes/musicnow/template-parts/hero.php
@@ -20,13 +20,15 @@ if ( function_exists( 'get_field' ) ) {
 				     data-flickity='<?php echo esc_attr( json_encode( $slider_settings ) ); ?>'>
 					<?php
 					foreach ( $slider as $slide ) {
+						_log( $slide );
 						// If there's no image, use placeholder image
 						$image_url = ( ! isset( $slide['image']['url'] ) ? 'https://placehold.it/1920x700' : $slide['image']['url'] );
 						// and placeholder text
 						$image_alt = ( ! isset( $slide['image']['url'] ) == null ? 'Image' : $image );
 						?>
 						<div class="carousel-cell">
-							<a href="<?php echo esc_url( $slide['link']['url'] ) ?>">
+							<a href="<?php echo esc_url( $slide['link']['url'] ) ?>"
+							   target="<?php echo esc_attr( $slide['link']['target'] ); ?>">
 								<img src="<?php echo esc_url( $image_url ); ?>"
 								     alt="<?php echo esc_attr( $image_alt ) ?>">
 							</a>

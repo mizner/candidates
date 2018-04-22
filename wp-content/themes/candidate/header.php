ee0ae23a5494b84d15752d21e941ee3421df0423
Mizner <mike@mizner.io>
2018-04-22 19:27:59 -0400

    header html cleanup

--- wp-content/themes/candidate/header.php
+++ wp-content/themes/candidate/header.php
@@ -3,6 +3,7 @@ get_template_part('head');
 do_action('candidate_before_header');
 
 use HG\CandidateCore\TemplateData\Header;
+use HG\CandidateCore\Utils\Esc;
 
 if (class_exists(Header::class)) :
     $data = new Header();
@@ -10,14 +11,18 @@ if (class_exists(Header::class)) :
     <header id="siteHeader" class="site-header">
         <div class="container">
             <div class="site-header__primary">
-                <?php if ($data->logo->url) : ?>
-                    <figure id="logo" class="site-header__logo">
-                        <img src="<?php echo esc_url($data->logo->url); ?>"
-                             alt="<?php echo esc_html(get_bloginfo('name')); ?>">
-                    </figure>
-                <?php else : ?>
-                    <h1 id="logo" class="site-header__logo-text"><?php echo esc_html(get_bloginfo('name')); ?></h1>
-                <?php endif; ?>
+                <a href="<?php echo Esc::url(get_home_url()); ?>">
+                    <?php if ($data->logo->url) : ?>
+                        <figure id="logo" class="site-header__logo">
+                            <img src="<?php echo Esc::url($data->logo->url); ?>"
+                                 alt="<?php echo Esc::title(get_bloginfo('name')); ?>">
+                        </figure>
+                    <?php else : ?>
+                        <h1 id="logo" class="site-header__logo-text">
+                            <span><?php echo Esc::title(get_bloginfo('name')); ?></span>
+                        </h1>
+                    <?php endif; ?>
+                </a>
             </div>
             <div class="site-header__secondary">
                 <div class="site-header__buttons">
@@ -25,14 +30,14 @@ if (class_exists(Header::class)) :
                         <button class="site-header__button button button__primary button__primary__ghosty"
                                 role="button"
                                 type="button">
-                            <span><?php echo esc_html__($data->buttons[0]->title, 'candidate'); ?></span>
+                            <span><?php echo Esc::title($data->buttons[0]->title, 'candidate'); ?></span>
                         </button>
                     </a>
                     <a href="<?php echo esc_url($data->buttons[1]->url); ?>">
                         <button class="site-header__button button button__secondary"
                                 role="button"
                                 type="button">
-                            <span><?php echo esc_html($data->buttons[1]->title, 'candidate'); ?></span>
+                            <span><?php echo Esc::title($data->buttons[1]->title, 'candidate'); ?></span>
                         </button>
                     </a>
                 </div>

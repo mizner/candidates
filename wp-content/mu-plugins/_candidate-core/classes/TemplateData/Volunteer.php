9d07b0e7135eda7a677104d0c7ea3d325b452931
Mizner <mike@mizner.io>
2018-04-22 23:25:09 -0400

    Fix background image url

--- wp-content/mu-plugins/_candidate-core/classes/TemplateData/Volunteer.php
+++ wp-content/mu-plugins/_candidate-core/classes/TemplateData/Volunteer.php
@@ -24,8 +24,12 @@ class Volunteer
         $prefix                 = 'volunteer_';
         $this->title            = get_the_title($id);
         $this->subtitle         = Meta::get($id, $prefix.'subtitle') ?: Fallback::title();
-        $this->background_image = Meta::get($id, $prefix.'background_image');
+        $background_image_id    = Meta::get($id, $prefix.'background_image');
         $this->description      = Meta::get($id, $prefix.'description') ?: Fallback::paragraph();
         $this->form             = Meta::get($id, $prefix.'form');
+
+        $this->background_image = $background_image_id ?
+            wp_get_attachment_image_url($background_image_id, 'full') :
+            Fallback::image('1920x700');
     }
 }

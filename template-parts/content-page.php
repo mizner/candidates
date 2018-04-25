4d5e6bd0cab61082b6918a9867169937000079e2
Mizner <mike@mizner.io>
2018-04-25 00:31:57 -0400

    remove stuff

--- template-parts/content-page.php
+++
@@ -1,6 +0,0 @@
-<section class="content-page">
-    <div class="container">
-        <h1><?php the_title(); ?></h1>
-        <?php the_content(); ?>
-    </div>
-</section>
--- template-parts/content-post.php
+++
@@ -1,6 +0,0 @@
-<section class="content-post">
-    <div class="container">
-        <h1><?php the_title(); ?></h1>
-        <?php the_content(); ?>
-    </div>
-</section>

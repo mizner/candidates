f726789e6c1a787faf7ae0b2903f5af6ff62394f
Erin Sunday <esundaydesign@gmail.com>
2018-04-25 00:15:14 -0400

    test

--- wp-content/themes/candidate/functions.php
+++ wp-content/themes/candidate/functions.php
@@ -4,7 +4,7 @@ namespace HG\Candidate;
 
 define(__NAMESPACE__.'\PATH', dirname(__FILE__).'/');
 define(__NAMESPACE__.'\URI', trailingslashit(get_template_directory_uri()));
-define(__NAMESPACE__.'\VERISON', '1.0.0');
+define(__NAMESPACE__.'\VERISON', '1.0.1');
 
 require_once 'lib/autoload.php';
 add_action('after_setup_theme', function () {
